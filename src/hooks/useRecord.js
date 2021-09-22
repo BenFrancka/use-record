01e5fcc4d82cb5fd786ac2db189470a72ba0e115
BenFrancka <benfrancka@gmail.com>
2021-09-22 16:15:45 -0700

    adds useRecord hook, might need some tweaking

--- src/hooks/useRecord.js
+++ src/hooks/useRecord.js
@@ -2,4 +2,37 @@ import { useState, useEffect } from 'react';
 
 export const useRecord = (init) => {
   const [current, setCurrent] = useState(init);
+  const [historyArray, setHistoryArray] = useState([init]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    setCurrentIndex(historyArray.indexOf(current));
+  }, [current]);
+
+  const record = (selectedValue) => {
+    setHistoryArray((previousArray) => [
+      ...previousArray.slice(0, currentIndex + 1),
+      selectedValue,
+      ...previousArray.slice(currentIndex + 1),
+    ]);
+
+    setCurrent(selectedValue);
+  };
+
+  const undo = () => {
+    setCurrent(historyArray[currentIndex - 1]);
+  };
+
+  const redo = () => {
+    setCurrent(historyArray + 1);
+  };
+
+  return {
+    current,
+    currentIndex,
+    historyArray,
+    undo,
+    redo,
+    record,
+  };
 };
