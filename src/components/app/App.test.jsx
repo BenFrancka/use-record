ccd1797aba5bd4ac5f19150259d759700a4714f2
BenFrancka <benfrancka@gmail.com>
2021-09-22 17:15:31 -0700

    redo button behavioral test passing

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -34,4 +34,23 @@ describe('Color Selection History', () => {
       expect(display).toHaveStyle({ 'background-color': '#00ff00' });
     });
   });
+
+  it('moves forward to previous selection when user clicks redo', () => {
+    render(<App />);
+
+    const undoButton = screen.getByLabelText('undo-button');
+    const redoButton = screen.getByLabelText('redo-button');
+    const colorInput = screen.getByLabelText('color-input');
+    const display = screen.getByLabelText('display');
+
+    fireEvent.change(colorInput, { target: { value: '#0000ff' } });
+    fireEvent.change(colorInput, { target: { value: '#00ff00' } });
+    fireEvent.change(colorInput, { target: { value: '#ffff00' } });
+    fireEvent.click(undoButton);
+    fireEvent.click(redoButton);
+
+    return waitFor(() => {
+      expect(display).toHaveStyle({ 'background-color': '#ffff00' });
+    });
+  });
 });
