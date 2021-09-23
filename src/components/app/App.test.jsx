dd7fca7acdac582d0c0e98dc1e4e5bdf0a9be0b8
BenFrancka <benfrancka@gmail.com>
2021-09-22 17:11:15 -0700

    undo behavioral test is passing

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -17,4 +17,21 @@ describe('Color Selection History', () => {
       expect(display).toHaveStyle({ 'background-color': '#ffff00' });
     });
   });
+
+  it('returns to previous selection when user clicks undo', () => {
+    render(<App />);
+
+    const undoButton = screen.getByLabelText('undo-button');
+    const colorInput = screen.getByLabelText('color-input');
+    const display = screen.getByLabelText('display');
+
+    fireEvent.change(colorInput, { target: { value: '#0000ff' } });
+    fireEvent.change(colorInput, { target: { value: '#00ff00' } });
+    fireEvent.change(colorInput, { target: { value: '#ffff00' } });
+    fireEvent.click(undoButton);
+
+    return waitFor(() => {
+      expect(display).toHaveStyle({ 'background-color': '#00ff00' });
+    });
+  });
 });
